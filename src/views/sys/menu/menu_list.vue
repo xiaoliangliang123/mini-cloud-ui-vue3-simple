188e043873ad61fbd83d960852ba3a206951371f
alan <1437033605@qq.com>
2023-02-06 22:17:25 +0800

    add menu views

--- src/views/sys/menu/menu_list.vue
+++ src/views/sys/menu/menu_list.vue
@@ -2,11 +2,11 @@
   <div class="common-layout">
     <el-container>
       <el-main>
-        <el-button type="success" style="margin-left: 15px" size="small" >新增</el-button>
+        <el-button type="success" style="margin-left: 15px" size="small" @click="handleAdd()">新增</el-button>
 
-        <el-tree ref="tree" style="margin-top: 10px"   :props="defaultProps"
+        <el-tree ref="tree" style="margin-top: 10px" :props="defaultProps"
                  node-key="orgId"
-                  lazy>
+                 lazy>
           <template #default="{ node }">
           <span class="custom-tree-node">
             <span><el-tag effect="plain">{{ node.label }}</el-tag></span>
@@ -22,15 +22,23 @@
 <script>
 export default {
   name: "menu_list",
-    data() {
-      return {
+  data() {
+    return {
 
-        defaultProps: {
-          children: 'children',
-          label: 'orgName'
-        }
+      defaultProps: {
+        children: 'children',
+        label: 'orgName'
       }
     }
+  },
+  methods: {
+    goTo(path) {
+      this.$router.push(path);
+    },
+    handleAdd() {
+      this.goTo('/menu/menu_edit/new');
+    }
+  }
 }
 </script>
 
