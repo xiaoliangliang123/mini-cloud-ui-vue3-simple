f4dfa5d1054f33b8ad365c0f93d41261fab0841d
alan <1437033605@qq.com>
2023-03-25 20:33:38 +0800

    add rolecodes  check

--- src/page/index/main.vue
+++ src/page/index/main.vue
@@ -18,12 +18,7 @@
           <!--          <el-menu-item index="2" >订单管理</el-menu-item> -->
           <!--          <el-menu-item index="3" >商品管理</el-menu-item>-->
           <!--          <el-menu-item index="4">统计监控</el-menu-item>-->
-          <el-menu-item index="10">
 
-            <span v-if="username=='游客'"><router-link to="/login">登录</router-link></span>
-            <span v-if="username!='游客'" @click="logout" style="color:#2d8cf0;text-decoration-line: underline">退出</span>
-
-          </el-menu-item>
         </el-menu>
       </el-header>
       <el-container>
@@ -55,9 +50,37 @@
         </el-main>
       </el-container>
       <el-footer class="foot-menu">
-        <span style="font-size: 22px;  max-width: 100%;margin-left: 30%">mini-cloud saas框架前端demo . VERSION: 1.0.0.0,当前登录归属：租户{{
-            tenantId
-          }}</span>
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content ep-bg-purple"/>
+          </el-col>
+          <el-col :span="14">
+            <div class="grid-content ep-bg-purple"/>
+            <span style="margin-bottom: 10%"> mini-cloud saas框架前端demo . VERSION: 1.0.0.0,当前登录归属：租户{{
+                tenantId
+              }}</span>
+          </el-col>
+          <el-col :span="4">
+            <div class="grid-content ep-bg-purple"/>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                <span style="color: goldenrod ">{{username}}</span>
+                <el-icon class="el-icon--right">
+                  <arrow-down/>
+                </el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item><span v-if="username=='游客'"><router-link to="/login">登录</router-link></span></el-dropdown-item>
+                  <el-dropdown-item><span v-if="username!='游客'" @click="logout" style="color:#2d8cf0;text-decoration-line: underline">退出</span></el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+        <!--        <span style="font-size: 22px;  max-width: 100%;margin-left: 30%">mini-cloud saas框架前端demo . VERSION: 1.0.0.0,当前登录归属：租户{{-->
+        <!--            tenantId-->
+        <!--          }}</span>-->
       </el-footer>
     </el-container>
   </div>
@@ -102,16 +125,20 @@ export default {
     },
     filterSubMenu(subMenus) {
       let authoritiesPaths = [];
-      if (store.state.user.userInfo.authorities == null || store.state.user.userInfo.authorities == 'undefined') {
-        return;
+      if (store.state.user.userInfo.authorities != null && store.state.user.userInfo.authorities != 'undefined') {
+        store.state.user.userInfo.authorities.forEach(function (item) {
+          authoritiesPaths.push(item.url);
+        });
       }
-      store.state.user.userInfo.authorities.forEach(function (item) {
-        authoritiesPaths.push(item.url);
-      });
+
       let smenus = subMenus.filter(function (subMenu) {
+
+        if(store.state.user.userInfo.roleCodes ==null ||store.state.user.userInfo.roleCodes =='undefined'){
+          return ;
+        }
         if (store.state.user.userInfo.roleCodes.indexOf("SUPER_ADMIN") > -1) {
-          return  subMenu.tag != 'indefined' && subMenu.tag == 'menu';
-        }else {
+          return subMenu.tag != 'indefined' && subMenu.tag == 'menu';
+        } else {
           return authoritiesPaths.indexOf(subMenu.path) != -1 && subMenu.tag != 'indefined' && subMenu.tag == 'menu';
 
         }
@@ -125,14 +152,17 @@ export default {
 
     const Router = useRouter();
     let authoritiesPaths = [];
-    store.state.user.userInfo.authorities.forEach(function (item) {
-      authoritiesPaths.push(item.url);
-    });
+
+    if (store.state.user.userInfo.authorities != null & store.state.user.userInfo.authorities != 'undefined') {
+      store.state.user.userInfo.authorities.forEach(function (item) {
+        authoritiesPaths.push(item.url);
+      });
+    }
+
     this.topMenus = Router.options.routes.filter(function (item) {
 
       console.log(store.state.user.userInfo.roleCodes);
       if (store.state.user.userInfo.roleCodes.indexOf("SUPER_ADMIN") > -1) {
-        debugger
         return (item.tag != 'indefined' && item.tag == 'top');
       }
       return (authoritiesPaths.indexOf(item.path) != -1 && item.tag != 'indefined' && item.tag == 'top');
@@ -143,18 +173,17 @@ export default {
     } else {
       this.leftMenus = this.currentTopMenu.children.filter(function (item) {
         return (authoritiesPaths.indexOf(item.path) != -1);
-    });
+      });
+    }
   }
-}
-,
-created()
-{
+  ,
+  created() {
 
-  if (store.state.user.userInfo.username != null) {
-    this.username = store.state.user.userInfo.username
-    this.tenantId = store.state.user.userInfo.tenantId
+    if (store.state.user.userInfo.username != null) {
+      this.username = store.state.user.userInfo.username
+      this.tenantId = store.state.user.userInfo.tenantId
+    }
   }
-}
 }
 </script>
 
